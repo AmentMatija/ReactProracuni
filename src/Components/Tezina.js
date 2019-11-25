54e915a597718070bc9490ee11da15eda1cf6b42
AmentMatija <44365140+AmentMatija@users.noreply.github.com>
2019-11-25 19:11:14 +0100

    promjena putanja komponenti

--- src/Components/Tezina.js
+++ src/Components/Tezina.js
@@ -1,26 +1,26 @@
 import {Switch, Route} from 'react-router-dom'
 
 import React,  {Component} from 'react';
-import TezKru from "./Components/Forme/TezKru";
-import TezKva from "./Components/Forme/TezKva";
-import TezCjev from "./Components/Forme/TezCjev";
-import OkrugliProfil from "./Components/Forme/OkrugliProfil";
-import TezPravokutneCjevi from "./Components/Forme/TezPravokutneCjevi";
-import PravokutniProfil from "./Components/Forme/PravokutniProfil";
-import TablicaMaterijala from "./Components/Forme/TablicaMaterijala";
+import TezKru from "./Forme/TezKru";
+import TezKva from "./Forme/TezKva";
+import TezCjev from "./Forme/TezCjev";
+import OkrugliProfil from "./Forme/OkrugliProfil";
+import TezPravokutneCjevi from "./Forme/TezPravokutneCjevi";
+import PravokutniProfil from "./Forme/PravokutniProfil";
+import TablicaMaterijala from "./Forme/TablicaMaterijala";
 
 class Tezina extends Component {
   render() {
       return (      
           <React.Fragment>             
               <Switch>                          
-                <Route exact path="/Components/Forme/TezKru" component={TezKru} />
-                <Route exact path="/Components/Forme/TezKva" component={TezKva} />
-                <Route exact path="/Components/Forme/Forme/TezCjev" component={TezCjev} />
-                <Route exact path="/Components/Forme/TezPravokutneCjevi" component={TezPravokutneCjevi} />
-                <Route exact path="/Components/Forme/OkrugliProfil" component={OkrugliProfil} />
-                <Route exact path="/Components/Forme/PravokutniProfil" component={PravokutniProfil} />
-                <Route exact path="/Components/Forme/TablicaMaterijala" component={TablicaMaterijala} />
+                <Route exact path="/Forme/TezKru" component={TezKru} />
+                <Route exact path="/Forme/TezKva" component={TezKva} />
+                <Route exact path="/Forme/Forme/TezCjev" component={TezCjev} />
+                <Route exact path="/Forme/TezPravokutneCjevi" component={TezPravokutneCjevi} />
+                <Route exact path="/Forme/OkrugliProfil" component={OkrugliProfil} />
+                <Route exact path="/Forme/PravokutniProfil" component={PravokutniProfil} />
+                <Route exact path="/Forme/TablicaMaterijala" component={TablicaMaterijala} />
               </Switch> 
             </React.Fragment>         
         
