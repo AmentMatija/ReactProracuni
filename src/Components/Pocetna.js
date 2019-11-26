d3b6769db4fa47a1e10baa5ce5527b3612b23b79
AmentMatija <44365140+AmentMatija@users.noreply.github.com>
2019-11-26 18:09:09 +0100

    Promjena pocetne stranice

--- src/Components/Pocetna.js
+++ src/Components/Pocetna.js
@@ -1,12 +1,17 @@
 import React,  {Component} from 'react';
 
+
 export default class Pocetna extends Component {
   render() {
 	    return (
-	    	<div>
-	   <h1>Ovo je početna stranica</h1>
+	      <div className="m-3">
+	   		<h1>Stranica je u izradi</h1>
+	   	
+	   		<p>Trenutno se može izračunati zapremnina cjevovoda te težina cjevi, ploča i punih profila.</p>
+	   		<p>Pri izračunu težine, zadana je težina lijevanog čelika, a u tablici gustoće materijala mogu se pronaći vrijednosti za druge, najčešće vrste materijala.</p>
+
 	  
-	   </div>
+	      </div>
 	   	);
-	 	}
 	}
+}
