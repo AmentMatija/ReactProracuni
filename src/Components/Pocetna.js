7f3b4a4316653ba3c3bfdcc9c8fce82b1941c459
AmentMatija <44365140+AmentMatija@users.noreply.github.com>
2019-11-26 18:12:52 +0100

    promjena teksta pocetne stranice

--- src/Components/Pocetna.js
+++ src/Components/Pocetna.js
@@ -8,7 +8,7 @@ export default class Pocetna extends Component {
 	   		<h1>Stranica je u izradi</h1>
 	   	
 	   		<p>Trenutno se može izračunati zapremnina cjevovoda te težina cjevi, ploča i punih profila.</p>
-	   		<p>Pri izračunu težine, zadana je težina lijevanog čelika, a u tablici gustoće materijala mogu se pronaći vrijednosti za druge, najčešće vrste materijala.</p>
+	   		<p>Pri izračunu težine, zadana je gustoća lijevanog čelika, a u tablici gustoće materijala mogu se pronaći vrijednosti za druge, najčešće vrste materijala.</p>
 
 	  
 	      </div>
